7c71f6445ead686c6fb7a22bb72f6ecc68c18550
james.marcogliese <james.marcogliese@madgex.com>
2019-01-28 20:50:54 -0500

    Added generated classes, specs, custom EventApi classes

---
+++ src/Slack.Api.CSharp/EventsApi/SlackEvent_Additions.cs
@@ -0,0 +1,41 @@
+﻿// Additions to the Auto-Generated SlackEvent_Generated class
+//
+
+
+namespace Slack.Api.CSharp.EventsApi
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    /// <summary>
+    /// Adapted from auto-generated content
+    /// </summary>
+    public partial class SlackEvent
+    {
+        [JsonProperty("challenge")]
+        public string Challenge { get; set; }
+    }
+
+    public partial class TheActualEventAnObjectThatHappened
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+        [JsonProperty("text")]
+        public string Text { get; set; }
+        [JsonProperty("channel")]
+        public string Channel { get; set; }
+        [JsonProperty("ts")]
+        public string Ts { get; set; }
+        [JsonProperty("channel_type")]
+        public string ChannelType { get; set; }
+        [JsonProperty("subtype")]
+        public string SubType { get; set; }
+        [JsonProperty("bot_id")]
+        public string BotId { get; set; }
+    }
+
+}
---
+++ test/Slack.Api.CSharp.Tests/UnitTests.cs
@@ -0,0 +1,17 @@
+using System.IO;
+//using FluentAssertions;
+using System.Threading.Tasks;
+using Xunit;
+using Newtonsoft.Json.Schema;
+
+namespace Slack.Api.CSharp.Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
